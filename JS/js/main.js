0c12879d79c7047ecc35d5bfce1b1cd4b0cc03c3
Bhuva Mihir <46589332+MihirBhuva@users.noreply.github.com>
2022-07-07 13:57:28 +0530

    Add files via upload

---
+++ JS/js/main.js
@@ -0,0 +1,20 @@
+function alertFunEx()
+{
+    alert('script');
+}
+function confirmFunEx()
+{
+    confirm('script');
+}
+function promptFunEx()
+{
+    prompt('script');
+}
+function background()
+{
+    document.body.style.backgroundColor=document.getElementById("back").value;
+}
+function divFun()
+{
+    document.getElementById('color').style.backgroundColor=document.getElementById("div").value;
+}
